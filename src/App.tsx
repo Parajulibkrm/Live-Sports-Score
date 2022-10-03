4b6ff62b38b850fc30c816ec1a1fdb5b3d74e90b
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-03 11:04:18 +0545

    Chore: Mantine Scafolded

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+import { MantineProvider, ColorSchemeProvider, createEmotionCache, ColorScheme } from "@mantine/core"
+import { ModalsProvider } from "@mantine/modals"
+import { NotificationsProvider } from "@mantine/notifications"
+import { SpotlightProvider } from "@mantine/spotlight"
 
-function App() {
-  const [count, setCount] = useState(0)
+// import { IconSun, IconMoonStars } from "@tabler/icons";
+import { useColorScheme, useLocalStorage } from "@mantine/hooks"
+import Dashboard from "./MainBody"
+import { IconSearch } from "@tabler/icons"
+import React from "react"
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const App: React.FC = () => {
+	const myCache = createEmotionCache({ key: "draww" })
+	const preferredColorScheme = useColorScheme()
+	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+		key: "draww-color-scheme",
+		defaultValue: preferredColorScheme,
+		getInitialValueInEffect: true
+	})
+	const toggleColorScheme = (value?: ColorScheme): void =>
+		setColorScheme(value ?? (colorScheme === "dark" ? "light" : "dark"))
+	//  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+	return (
+		<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+			<MantineProvider
+				emotionCache={myCache}
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{ colorScheme, fontFamily: "Poppins" }}
+			>
+				<ModalsProvider>
+					<NotificationsProvider>
+						<SpotlightProvider
+							shortcut={["mod + P"]}
+							actions={[]}
+							searchIcon={<IconSearch size={18} />}
+							searchPlaceholder="Search Anything..."
+							nothingFoundMessage="Nothing found..."
+						>
+							<Dashboard />
+							{/* </ActionIcon> */}
+						</SpotlightProvider>
+					</NotificationsProvider>
+				</ModalsProvider>
+			</MantineProvider>
+		</ColorSchemeProvider>
+	)
 }
 
 export default App
