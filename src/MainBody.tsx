699cbf708fcf3848d3ff0134b320d9a249fe67b1
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-04 14:29:30 +0545

    Fix: Fixed emotion left padding issue on mobile

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -1,10 +1,18 @@
-import { AppShell, Aside, Center, Loader, MediaQuery, Text } from "@mantine/core";
+import {
+  AppShell,
+  Aside,
+  Center,
+  Loader,
+  MediaQuery,
+  Text,
+} from "@mantine/core";
 import React, { lazy, Suspense } from "react";
 // import ExcalidrawApp from "./Excalidraw";
 // import Ex from "./Ex";
 import NavbarSearch from "./Components/NavBarWithSearch";
 import { atom } from "jotai";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./Pages/Game";
 // import Home from "./Pages/Home";
 export const open = atom(true);
 
@@ -13,30 +21,42 @@ const Home = lazy(async () => await import("./Pages/Home"));
 const MainBody = (): JSX.Element => {
   return (
     <Router>
-      <AppShell
-        navbarOffsetBreakpoint="sm"
-        navbar={<NavbarSearch />}
-        aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      >
-        <Suspense
-          fallback={
-            <Center>
-              <Loader variant="dots" />
-            </Center>
+      <MediaQuery smallerThan={"sm"} styles={{ paddingLeft: 0 }}>
+        <AppShell
+          navbarOffsetBreakpoint="sm"
+          navbar={<NavbarSearch />}
+          aside={
+            <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+              <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+                <Text>Application sidebar</Text>
+              </Aside>
+            </MediaQuery>
           }
+          styles={(theme) => ({
+            main: {
+              // padding: 0,
+              [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+                padding: 0,
+              },
+            },
+          })}
+          // sx={{ margin: 0, padding: 0 }}
         >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/list" element={<Home />} />
-          </Routes>
-        </Suspense>
-      </AppShell>
+          <Suspense
+            fallback={
+              <Center>
+                <Loader variant="dots" />
+              </Center>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/list" element={<Home />} />
+              <Route path="/game" element={<Game />} />
+            </Routes>
+          </Suspense>
+        </AppShell>
+      </MediaQuery>
     </Router>
   );
 };
