cf44c123e2a5d36ee1e5eadc9f566b3450ca2e7f
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-04 14:26:03 +0545

    Feat: Added a basic card to display live match

---
+++ src/Components/GameCard.tsx
@@ -0,0 +1,96 @@
+import {
+  Badge,
+  Center,
+  createStyles,
+  Group,
+  Image,
+  Space,
+  Text,
+} from "@mantine/core";
+import React from "react";
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+  card: {
+    backgroundColor: "#9272EA",
+  },
+  title: {
+    color: theme.white,
+    textTransform: "uppercase",
+    fontWeight: 700,
+    fontSize: theme.fontSizes.sm,
+  },
+  goals: {
+    color: theme.white,
+    fontWeight: 700,
+    fontSize: theme.fontSizes.xl,
+  },
+
+  count: {
+    color: theme.white,
+    fontSize: 32,
+    lineHeight: 1,
+    fontWeight: 700,
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
+
+  description: {
+    color: theme.colors[theme.primaryColor][0],
+    fontSize: theme.fontSizes.sm,
+    marginTop: 2,
+  },
+}));
+
+const GameCard = () => {
+  const { classes } = useStyles();
+  return (
+    <>
+      <Group position="apart">
+        <Group align={"flex-end"}>
+          <Text className={classes.title}>Hello World</Text>
+        </Group>
+        <Group>
+          <Badge>live</Badge>
+        </Group>
+      </Group>
+      {/* <Text size="sm" className={classes.description}>
+              Page views
+            </Text> */}
+      <Space h={"md"} />
+      <Group position="apart">
+        <div>
+          <Center>
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg"
+              width={80}
+            />
+          </Center>
+          <Center>
+            <Text className={classes.description}>Chelsea</Text>
+          </Center>
+        </div>
+        <div>
+          <Center>
+            <Text className={classes.goals}>12:01</Text>
+          </Center>
+          <Center>
+            <Badge color={"red"}>87'</Badge>
+          </Center>
+        </div>
+        <div>
+          <Center>
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg"
+              width={80}
+            />
+          </Center>
+          <Center>
+            <Text className={classes.description}>Chelsea</Text>
+          </Center>
+        </div>
+      </Group>
+    </>
+  );
+};
+
+export default GameCard;
