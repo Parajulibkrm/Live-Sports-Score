110d84d5f1a4bd0941c8f6f4abb620c23a2ae4b9
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-04 14:30:46 +0545

    Feat: Put together a basic home screen layout

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,65 @@
+import { Avatar, Badge, Group, ScrollArea } from "@mantine/core";
+import React, { useEffect, useRef, useState } from "react";
+import SportsPill from "../Components/SportsPill";
+import autoAnimate from "@formkit/auto-animate";
+import CarouselGames from "../Components/CarouselGames";
+import MatchList from "../Components/MatchList";
+
+const SPORTS = [
+  {
+    title: "Football",
+    img: "https://freepngdownload.com/image/thumb/download-football-png.png",
+    slug: "football",
+  },
+  {
+    title: "Basketball",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Basketball_Clipart.svg/400px-Basketball_Clipart.svg.png?20210111192439",
+    slug: "basketball",
+  },
+  {
+    title: "Cricket",
+    img: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Cricketball.svg",
+    slug: "cricket",
+  },
+];
+const Home = () => {
+  const [selected, setSelected] = useState(0);
+  const parentRef = useRef(null);
+
+  useEffect(() => {
+    if (parentRef.current) {
+      autoAnimate(parentRef.current);
+    }
+  }, [parent]);
+
+  return (
+    <>
+      <div>Home</div>
+      <ScrollArea
+        offsetScrollbars
+        sx={{ width: "100%" }}
+        styles={(_) => ({
+          scrollbar: {
+            visibility: "hidden",
+          },
+        })}
+      >
+        <Group ref={parentRef} style={{ width: "150%" }}>
+          {SPORTS.map((sport, index) => (
+            <div onClick={() => setSelected(index)} key={sport.slug}>
+              <SportsPill
+                src={sport.img}
+                isSelected={selected === index}
+                title={sport.title}
+              />
+            </div>
+          ))}
+        </Group>
+      </ScrollArea>
+      <CarouselGames />
+      <MatchList />
+    </>
+  );
+};
+
+export default Home;
