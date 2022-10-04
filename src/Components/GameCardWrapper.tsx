2bc8e400bb12ece328f3a805fcb8e87efc151748
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-04 17:55:31 +0545

    Feat: Added Game Title to Game Card

--- src/Components/GameCardWrapper.tsx
+++ src/Components/GameCardWrapper.tsx
@@ -1,4 +1,4 @@
-import { Card, Center, createStyles, Text } from "@mantine/core";
+import { Badge, Card, Center, createStyles, Group, Text } from "@mantine/core";
 import React from "react";
 import { Link } from "react-router-dom";
 import GameCard from "./GameCard";
@@ -11,6 +11,12 @@ const useStyles = createStyles((theme, _params, getRef) => ({
     color: theme.colors[theme.primaryColor][0],
     fontSize: theme.fontSizes.sm,
     marginTop: 2,
+  },
+  title: {
+    color: theme.white,
+    textTransform: "uppercase",
+    fontWeight: 700,
+    fontSize: theme.fontSizes.sm,
   },
 }));
 
@@ -27,6 +33,14 @@ const GameCardWrapper = () => {
       component={Link}
       to="/game"
     >
+      <Group position="apart">
+        <Group align={"flex-end"}>
+          <Text className={classes.title}>Hello World</Text>
+        </Group>
+        <Group>
+          <Badge>live</Badge>
+        </Group>
+      </Group>
       <GameCard />
       <Center>
         <Text className={classes.description} size="sm">
