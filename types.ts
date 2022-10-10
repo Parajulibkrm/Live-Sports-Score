abc63396f0a1817d4d0b1c6948f6a13221a2f9fa
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-10 13:20:02 +0545

    Chore: Added types

---
+++ types.ts
@@ -0,0 +1,50 @@
+// Tournament > Matches
+// Match > Select Teams 
+// Create Team 
+// Create Player - name,
+//     Team has players
+// Match - date, title, desc, venue, status
+//     - Teams - players, title, logo
+//     - Events - type, player1, player2, timestamp
+export interface Player {
+    key?: string;
+    name: string;
+    photoUrl?: string;
+}
+
+export interface Team {
+    key?: string;
+    name: string;
+    logo?: string;
+}
+export interface Tournament {
+    key?: string;
+    title: string;
+    logo: string;
+}
+export interface Match {
+    key?: string;
+    title: string;
+    tournament?: string;
+    desc: string;
+    venue: string;
+    time: number;
+    status: 'SCHEDULED' | 'STARTED' | 'CANCELED' | 'POSTPONED' | 'PAUSED' | 'ENDED';
+    events: string[];
+    teams: string[];
+    goals: [{ player: string, assist: string, timeStamp: number, matchTimeStamp: number, team: string }]
+}
+export interface Event {
+    type: string;
+    comment: string;
+    timeStamp: string;
+    matchTimeStamp: string;
+    player1?: string;
+    player2?: string;
+    team1?: string;
+    team2?: string;
+}
+
+export type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
