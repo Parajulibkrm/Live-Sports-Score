86c04b4e06fd6a55be3b0a48d33fcf3fad6156d1
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-10 13:21:22 +0545

    Refactor: Removed Dead Code

--- src/Components/HeaderMiddle.tsx
+++ src/Components/HeaderMiddle.tsx
@@ -43,7 +43,7 @@ const useStyles = createStyles((theme) => ({
 
     [theme.fn.smallerThan("sm")]: {
       width: "auto",
-      marginLeft: "auto",
+      marginRight: "auto",
     },
   },
 
@@ -52,38 +52,6 @@ const useStyles = createStyles((theme) => ({
 
     [theme.fn.largerThan("sm")]: {
       display: "none",
-    },
-  },
-
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "8px 12px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-        .color,
     },
   },
 }));
@@ -115,36 +83,7 @@ export function HeaderMiddle(): JSX.Element {
       style={{ margin: 0 }}
       position="apart"
     >
-      <Group className={classes.links}>
-        <a
-          href={"/"}
-          className={classes.link}
-          onClick={(event) => {
-            event.preventDefault();
-          }}
-        >
-          Home
-        </a>
-      </Group>
-      <Burger
-        opened={opened}
-        onClick={() => setOpened((o) => !o)}
-        size="sm"
-        className={classes.burger}
-      />
-
-      <Image
-        src={dark ? "Draww-dark-cover.png" : "Draww-cover.png"}
-        style={{ maxHeight: 50, maxWidth: 100 }}
-      />
-
-      <Group spacing={4} className={classes.social} position="right" noWrap>
-        <ActionIcon size="lg" onClick={() => openSpotlight()}>
-          <IconSearch size={18} stroke={1.5} />
-        </ActionIcon>
-        <ActionIcon size="lg">
-          <IconBrandGithub size={18} stroke={1.5} />
-        </ActionIcon>
+      <Group spacing={4} className={classes.social} position="left">
         <ActionIcon
           variant="outline"
           color={dark ? "yellow" : "blue"}
@@ -157,10 +96,28 @@ export function HeaderMiddle(): JSX.Element {
             <IconMoonStars size={18} stroke={1.5} />
           )}
         </ActionIcon>
+        <ActionIcon size="lg">
+          <IconBrandGithub size={18} stroke={1.5} />
+        </ActionIcon>
+
+        <ActionIcon size="lg" onClick={() => openSpotlight()}>
+          <IconSearch size={18} stroke={1.5} />
+        </ActionIcon>
         {/* <Avatar src={"Test"} radius="xl" color={"blue"}>
           BP
         </Avatar> */}
       </Group>
+      <Image
+        src={dark ? "Draww-dark-cover.png" : "Draww-cover.png"}
+        style={{ maxHeight: 50, maxWidth: 100 }}
+      />
+
+      <Burger
+        opened={opened}
+        onClick={() => setOpened((o) => !o)}
+        size="sm"
+        className={classes.burger}
+      />
     </Group>
   );
 }
