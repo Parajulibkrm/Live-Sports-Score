4a5dbfc2555926cee8b85a1482783b0fb0a63035
Parajulibkrm <bikramparajuli0000@gmail.com>
2022-10-10 13:31:21 +0545

    Feat: Flexible Height, Some minor changes, divider

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,31 +1,31 @@
-import { Avatar, Badge, Container, Group, ScrollArea } from "@mantine/core";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Divider,
+  Group,
+  ScrollArea,
+  Space,
+  Text,
+} from "@mantine/core";
 import React, { useEffect, useRef, useState } from "react";
-import SportsPill from "../Components/SportsPill";
+import TournamentPill from "../Components/TournamentPill";
 import autoAnimate from "@formkit/auto-animate";
 import CarouselGames from "../Components/CarouselGames";
 import MatchListItem from "../Components/MatchListItem";
 import HeaderMiddle from "../Components/HeaderMiddle";
-
-const SPORTS = [
-  {
-    title: "Football",
-    img: "https://freepngdownload.com/image/thumb/download-football-png.png",
-    slug: "football",
-  },
-  {
-    title: "Basketball",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Basketball_Clipart.svg/400px-Basketball_Clipart.svg.png?20210111192439",
-    slug: "basketball",
-  },
-  {
-    title: "Cricket",
-    img: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Cricketball.svg",
-    slug: "cricket",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { Tournament } from "../../types";
 const Home = () => {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState<string | null>(null);
   const parentRef = useRef(null);
+  const {
+    isLoading,
+    error,
+    data: tournaments,
+  } = useQuery(["leagues"], () =>
+    fetch("http://localhost:5000/tournament").then((res) => res.json())
+  );
 
   useEffect(() => {
     if (parentRef.current) {
@@ -35,46 +35,81 @@ const Home = () => {
 
   return (
     <>
-      <Container
+      <Box
         sx={(theme) => ({
           padding: 0,
+          display: "flex",
+          flexDirection: "column",
           [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
             padding: 8,
           },
+          height: "100vh",
+          marginBottom: 15,
         })}
       >
         <HeaderMiddle />
-        <ScrollArea
-          offsetScrollbars
-          sx={{ width: "100%" }}
-          styles={(_) => ({
-            scrollbar: {
-              visibility: "hidden",
-            },
-          })}
-        >
-          <Group ref={parentRef} style={{ width: "150%" }}>
-            {SPORTS.map((sport, index) => (
-              <div onClick={() => setSelected(index)} key={sport.slug}>
-                <SportsPill
-                  src={sport.img}
-                  isSelected={selected === index}
-                  title={sport.title}
+        <Space h={10} />
+        <div>
+          <ScrollArea
+            offsetScrollbars
+            sx={{ width: "100%" }}
+            styles={(_) => ({
+              scrollbar: {
+                visibility: "hidden",
+              },
+            })}
+          >
+            <Group
+              ref={parentRef}
+              style={{ overflowX: "scroll", display: "table" }}
+            >
+              <div
+                onClick={() => setSelected(null)}
+                style={{ display: "table-cell", marginRight: 7 }}
+              >
+                <TournamentPill
+                  src={
+                    "https://upload.wikimedia.org/wikipedia/commons/d/d3/Soccerball.svg"
+                  }
+                  isSelected={selected === null}
+                  title={`All`}
                 />
               </div>
-            ))}
-          </Group>
+
+              {tournaments &&
+                tournaments.items.map((tournament: Tournament) => (
+                  <div
+                    onClick={() => setSelected(tournament?.key || "")}
+                    key={tournament.key}
+                    style={{ display: "table-cell" }}
+                  >
+                    <TournamentPill
+                      src={tournament.logo}
+                      isSelected={selected === tournament.key}
+                      title={tournament.title}
+                    />
+                  </div>
+                ))}
+            </Group>
+          </ScrollArea>
+        </div>
+        <Divider my={10} label="Live Matches" />
+        <div>
+          <CarouselGames />
+        </div>
+        <Divider my={10} label="Upcoming Matches" />
+        <ScrollArea sx={{ flexGrow: 1 }}>
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
+          <MatchListItem />
         </ScrollArea>
-        <CarouselGames />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-        <MatchListItem />
-      </Container>
+        {/* <Text>Test</Text> */}
+      </Box>
     </>
   );
 };
